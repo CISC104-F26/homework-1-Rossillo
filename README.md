30dcb0a5aa73c854ecfe871c57f93aaed5cac474
Lochlan Belford <142742786+lbelford-udel@users.noreply.github.com>
2026-08-26 21:38:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# homework-1-template
+Template Godot project for Homework 1: Hello, World!
---
+++ homework-1-hello-world/HelloWorld.gd
@@ -0,0 +1,40 @@
+class_name HelloWorld extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Welcome to Homework 1!")
+	
+	"""
+	Part A:
+	- Write your line of code on the blank lines
+		between the dashed lines below.
+        
+	- Remember: indentation matters! The first letter
+		of your line of code should be lined up with the
+		# symbol at the start of the dashed lines
+    """ 
+	#---------------------
+	
+	#----------------------
+
+	"""
+	Part B:
+		-As before, write your code on the blank line
+		in between the dashed lines
+		
+		- Remember: indentation matters!
+	"""
+	#---------------------
+	
+	#----------------------
+	
+	"""
+	Part C:
+		-One more time, write your code in the
+		provided blank space
+	"""
+	#---------------------
+	
+	#----------------------
+	
